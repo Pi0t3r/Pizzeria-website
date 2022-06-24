1db1d03c5a76d1ce1fad557c9e61de4e6c3738d0
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-06-25 01:26:13 +0200

    second commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.navi')
+const btn = document.querySelector('.bars')
+const allNavItems = document.querySelectorAll('.nav-items ul li')
+
+
+// const handleNav = () => {
+//     nav.classList.toggle('active')
+//     allNavItems.forEach(item -> {
+    
+//     })
+// }
