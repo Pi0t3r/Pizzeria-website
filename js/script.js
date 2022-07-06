c4419a9b833c69f4943f6849148aaf456594257b
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-07-06 13:59:39 +0200

    repair navigation list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navi = document.querySelector('.navi')
 const btn = document.querySelector('.fa-bars')
 const close = document.querySelector('.fa-xmark')
+const allNavItems = document.querySelectorAll('.effect')
 
 const moveNav = () => {
     navi.classList.add('active')
@@ -8,6 +9,19 @@ const moveNav = () => {
 const closeNav = () => {
     navi.classList.remove('active')
 }
+
+
+
+const handleNav = () => {
+    navi.classList.toggle('active')
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () =>{
+            navi.classList.remove('active')
+        })
+    })
+    
+}
+
 
 btn.addEventListener('click',moveNav)
 close.addEventListener('click',closeNav)
